b5b4bff39cd2a22b592b63da7f668ecd488ff2ef
Wu Haotian <whtsky@gmail.com>
2019-03-12 15:21:42 +0800

    feat: argparse

--- pack.py
+++ pack.py
@@ -1,26 +1,41 @@
+#!/usr/bin/env python3
 import sys
 import re
 import zipfile
 import tempfile
+import argparse
 import itertools
 
 from pathlib import Path
 
-SUPPORTED_IMG_EXTENSIONS = ['jpg', 'jpeg', 'png']
+parser = argparse.ArgumentParser()
+parser.add_argument("base_path", type=str, help="")
+parser.add_argument("-o", "--output_path", help="where to store generated zip files")
+parser.add_argument(
+    "-e",
+    "--episode-per-pack",
+    type=int,
+    help="how many episode per zip pack",
+    default=5,
+)
 
-if len(sys.argv) < 2:
-    print("Usage: {} <base_path> [output_path=base_path] [episode-per-pack=5]".format(sys.argv[0]))
-    sys.exit()
+args = parser.parse_args()
+print(args)
+
+SUPPORTED_IMG_EXTENSIONS = ["jpg", "jpeg", "png"]
 
 episode_number = re.compile("(\d+)")
+
+
 def get_episode_number(path: Path):
     return episode_number.findall(path.name)[0]
 
-base_path = Path(sys.argv[1])
-output_path = len(sys.argv) >= 3 and Path(sys.argv[2]) or base_path
+
+base_path = Path(args.base_path)
+output_path = args.output_path and Path(args.output_path) or args.base_path
 output_path.mkdir(exist_ok=True)
 
-episoide_per_pack = len(sys.argv) == 4 and int(sys.argv[3]) or 5
+episoide_per_pack = args.episode_per_pack
 
 
 def glob_images(path: Path):
@@ -39,23 +54,27 @@ def grouper(folders):
 def check_image_file_length(folders):
     files = list(itertools.chain(*map(glob_images, folders)))
     if len(set(map(len, [x.stem for x in files]))) not in (0, 1):
-        raise Exception('文件名数字序号长度需要保持一致\n%s' % '\n'.join(map(str, files)))
+        raise Exception("文件名数字序号长度需要保持一致\n%s" % "\n".join(map(str, files)))
 
 
-folders = sorted([x for x in base_path.iterdir() if x.is_dir() and not x.name.startswith('.')])
+folders = sorted(
+    [x for x in base_path.iterdir() if x.is_dir() and not x.name.startswith(".")]
+)
 check_image_file_length(folders)
 for group in grouper(folders):
     print(group)
-    pack_name = "{}-{}".format(get_episode_number(group[0]), get_episode_number(group[-1]))
+    pack_name = "{}-{}".format(
+        get_episode_number(group[0]), get_episode_number(group[-1])
+    )
     print("Creating {}.zip".format(pack_name))
     zip_path = output_path / "{}.zip".format(pack_name)
-    with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zf:
+    with zipfile.ZipFile(zip_path, "w", zipfile.ZIP_DEFLATED) as zf:
         navpoints = []
         for ep_folder in group:
             files = glob_images(ep_folder)
             # Add images
             for file in files:
-                zf.write(str(file), ep_folder.name + '/' + file.name)
+                zf.write(str(file), ep_folder.name + "/" + file.name)
             # Add navpoint
             navpoints.append("{0},{0}={1}".format(ep_folder.name, files[0].name))
-        zf.writestr('vol-navpoint.txt', '\n'.join(navpoints))
+        zf.writestr("vol-navpoint.txt", "\n".join(navpoints))
